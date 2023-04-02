5e41db3702b26a15fce1c350044374e5c7d7533b
Daniel Greaves <dgreaves18@gmail.com>
2023-04-02 00:23:20 -0700

    More setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '/client/src/index.jsx'),
+  output: {
+    path: path.join(__dirname, '/'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      },
+    ],
+  },
+};
