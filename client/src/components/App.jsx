986c025ba5cfb17a0a7583ce56a5d929d30ae1cb
Daniel Greaves <dgreaves18@gmail.com>
2023-04-05 22:03:14 -0700

    added nav bar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,8 +2,29 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      Buhbuhbuh
+    <div id="App">
+      <ul id="navbar">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#skills">My skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div id="home">
+        Home here
+      </div>
+      <div id="about">
+        About Me here
+      </div>
+      <div id="skills">
+        My skills here
+      </div>
+      <div id="projects">
+        Projects here
+      </div>
+      <div id="contact">
+        Contact here
+      </div>
     </div>
   );
 }
