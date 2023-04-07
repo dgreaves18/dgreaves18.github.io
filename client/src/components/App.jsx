d23e93bd2d78fcbcce830a7cdaf1ffed14cd6892
Daniel Greaves <dgreaves18@gmail.com>
2023-04-06 22:05:01 -0700

    corrected navbar order

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,11 +4,11 @@ function App() {
   return (
     <div id="App">
       <ul id="navbar">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About Me</a></li>
-        <li><a href="#skills">My skills</a></li>
-        <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#skills">My skills</a></li>
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#home">Home</a></li>
       </ul>
       <div id="home">
         Home here
