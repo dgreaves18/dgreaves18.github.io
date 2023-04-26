3fd59e00c12ec82610bc579f404a33cbb72ed5c8
Daniel Greaves <dgreaves18@gmail.com>
2023-04-25 22:26:42 -0700

    updated text size and spacing, added info bubble

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [displayInfo, setDisplayInfo] = useState(false);
+
   return (
     <div id="App">
       <div id="home">
-        Welcome
-        <div class="nav-buttons">
-          <a href="#about"><button class="nav-button">About Me</button></a>
-          <a href="#skills"><button class="nav-button">My skills</button></a>
-          <a href="#projects"><button class="nav-button">Projects</button></a>
-          <a href="#contact"><button class="nav-button">Contact</button></a>
+        <span id="hello">Hello</span>
+        <div className="nav-buttons">
+          <a href="#about"><button className="nav-button">About Me</button></a>
+          <a href="#skills"><button className="nav-button">My Skills</button></a>
+          <a href="#projects"><button className="nav-button">My Projects</button></a>
+          <a href="#contact"><button className="nav-button">Contact Me</button></a>
         </div>
+        <div id="home-info" onClick={(e) => {setDisplayInfo(!displayInfo)}}>?</div>
+        {displayInfo && (
+          <span id="info">Rifle Peak, Incline Village, NV</span>
+        )}
       </div>
       <div id="about">
-        <div class="img-crop">
-          <img class="profile" src="client/lib/Headshot_resized.jpg"></img>
+        <div className="img-crop">
+          <img className="profile" src="client/lib/Headshot_resized.jpg"></img>
         </div>
         About Me here
       </div>
-      <div class="bg">
+      <div className="bg">
         <div id="skills">
           My skills here
         </div>
@@ -28,8 +34,8 @@ function App() {
       </div>
       <div id="contact">
         Contact here
-        <div class="nav-buttons">
-          <a href="#home"><button class="nav-button">Back to Top</button></a>
+        <div className="nav-buttons">
+          <a href="#home"><button className="nav-button">Back to Top</button></a>
         </div>
       </div>
     </div>
