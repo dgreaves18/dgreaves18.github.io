3a21fe1b46bde59cef4cc6808ac401e107479949
Daniel Greaves <dgreaves18@gmail.com>
2023-04-25 19:57:35 -0700

    update bg, added nav buttons and profile pic

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,26 +3,19 @@ import React from 'react';
 function App() {
   return (
     <div id="App">
-      <div class="hamburger-menu">
-        <input id="menu__toggle" type="checkbox" />
-        <label class="menu__btn" for="menu__toggle">
-          <span></span>
-        </label>
-
-        <ul class="menu__box">
-          <li><a class="menu__item" href="#home">Home</a></li>
-          <li><a class="menu__item" href="#about">About Me</a></li>
-          <li><a class="menu__item" href="#skills">My skills</a></li>
-          <li><a class="menu__item" href="#projects">Projects</a></li>
-          <li><a class="menu__item" href="#contact">Contact</a></li>
-        </ul>
-      </div>
-      <div class="bg">
-        <div id="home">
-          Welcome
+      <div id="home">
+        Welcome
+        <div class="nav-buttons">
+          <a href="#about"><button class="nav-button">About Me</button></a>
+          <a href="#skills"><button class="nav-button">My skills</button></a>
+          <a href="#projects"><button class="nav-button">Projects</button></a>
+          <a href="#contact"><button class="nav-button">Contact</button></a>
         </div>
       </div>
       <div id="about">
+        <div class="img-crop">
+          <img class="profile" src="client/lib/Headshot_resized.jpg"></img>
+        </div>
         About Me here
       </div>
       <div class="bg">
@@ -35,6 +28,9 @@ function App() {
       </div>
       <div id="contact">
         Contact here
+        <div class="nav-buttons">
+          <a href="#home"><button class="nav-button">Back to Top</button></a>
+        </div>
       </div>
     </div>
   );
